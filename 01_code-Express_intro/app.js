c1c84563ea70f7bafb2d2c6dca75a227f380d38d
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-06-22 10:10:40 +0200

    Basic setup

---
+++ 01_code-Express_intro/app.js
@@ -0,0 +1,11 @@
+/**
+ * TOC for Express Intro code along
+ * 
+ * 1. mpm init -> initialize your node environment and packager
+ * 2. npm install express
+ * 3. Import the newly installed express module (library)
+ * 4. Setup the express.app()
+ * 5. Tell express.app() what it need to respond to (set a route)
+ * 6. Prepare the .html ang .img resourses
+ * 7. Sart the express.app() so it "listens" to a port
+ */
